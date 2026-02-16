b229c4c3bd6935f0331e31ca611773989bde70f3
Laharivanapalli <laharivanapallii@gmail.com>
2026-02-16 10:01:34 +0530

    this is project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Count {count}</h1>
+
+        <div>
+          <button onClick={() => setCount(count + 1)}>Increase</button>
+          <button onClick={() => setCount(count - 1)}>decrease</button>
+          <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
